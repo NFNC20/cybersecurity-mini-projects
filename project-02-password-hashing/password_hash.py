8eb00884598327cc8b51ce1e98c77954f6f290d6
NFNC.20 <feyzacelik1@web.de>
2025-12-18 20:21:10 +0100

    Fix typo in password input prompt

--- project-02-password-hashing/password_hash.py
+++ project-02-password-hashing/password_hash.py
@@ -3,7 +3,7 @@ import hashlib
 
 print("Password Hashing Demo\n")
 
-password = input("Enter a password to hast: ")
+password = input("Enter a password to hash: ")
 
 # SHA-256 Hash erzeugen
 hashed = hashlib.sha256(password.encode()).hexdigest()
